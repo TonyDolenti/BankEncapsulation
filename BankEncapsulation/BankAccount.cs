1e34072c1ae9f855ea409670ff6ef619ecabff66
TonyDolenti <antoniobdolenti@gmail.com>
2023-02-16 20:29:42 -0500

    inital commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public double Deposit(double depositamount)
+        {
+            return _balance += depositamount;
+            
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var bankAccount = new BankAccount();
+            Console.WriteLine($"Current Balance is: {bankAccount.GetBalance()}");
+            Console.WriteLine("How much would you like to deposit?");
+            Double.TryParse(Console.ReadLine(), out var usermoney);
+
+            bankAccount.Deposit(usermoney);
+            Console.WriteLine($"Current balance is now: {bankAccount.GetBalance()}");
         }
     }
 }
